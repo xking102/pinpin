7dc741b7f44cc3807ffd7afa8936a2bc4c852b3c
XingKaiXin <xingkaixin@gmail.com>
2015-05-21 11:37:13 +0800

    add chinese label modify the create_userid

--- form/group/group.py
+++ form/group/group.py
@@ -6,21 +6,21 @@ from flask_wtf import Form
 from flask_wtf.html5 import EmailField
 from wtforms import StringField, PasswordField, SubmitField,validators, \
 					TextAreaField, DecimalField, IntegerField
-from wtforms.validators import DataRequired, Email, InputRequired
+from wtforms.validators import DataRequired, Email, InputRequired, NumberRange
 from module.group.group import Group
 from control import pinpin
 from control.pinpin import statusRef
 from app import db
 
 class newGroupForm(Form):
-	title = StringField('title', [InputRequired()])
-	desc = TextAreaField('desc', [InputRequired()])
-	unit_price = DecimalField('unit_price', [InputRequired()])
-	list_price = DecimalField('list_price', [InputRequired()])
-	total_qty = IntegerField('total_qty', [InputRequired()])
-	submit = SubmitField('submit')
+	title = StringField(u'标题', [InputRequired()])
+	desc = TextAreaField(u'描述', [InputRequired()])
+	unit_price = DecimalField(u'到手单价', [InputRequired(),NumberRange(min=0,max=None,message='price >0')])
+	list_price = DecimalField(u'原价', [InputRequired(),NumberRange(min=0,max=None,message='price >0')])
+	total_qty = IntegerField(u'购买总数', [InputRequired(),NumberRange(min=1,max=None,message='qty>=1')])
+	submit = SubmitField(u'发布')
 
-	def validate(self, field):
+	def validate_title(self, field):
 		title = self.title.data
 		desc = self.desc.data
 		unit_price = self.unit_price.data
@@ -33,7 +33,7 @@ class newGroupForm(Form):
 		group.list_price = list_price
 		group.total_qty = total_qty
 		group.create_dt = pinpin.getCurTimestamp()
-		gropu.create_userid = session['logged_id']
+		group.create_userid = session.get('logged_id')
 		group.update_dt = pinpin.getCurTimestamp()
 		group.status = statusRef.GROUP_PUBLISH
 		group.save
