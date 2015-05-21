bccdb5f5b67f23ed37fbb214116e1ee124247a42
XingKaiXin <xingkaixin@gmail.com>
2015-05-21 16:37:59 +0800

    remove var groups~

--- static/js/GroupsApp/GroupsApp.js
+++ static/js/GroupsApp/GroupsApp.js
@@ -5,18 +5,6 @@ var Pager =  require("./Pager");
 
 module.exports = React.createClass({
 	getInitialState:function(){
-		var groups = [
-			{
-				id: 1,
-				img: '/static/imgs/2.png',
-				title:'#1新一轮日亚化妆品海淘开始，姐妹们快快加入xingkaixin你很厉害啊吧新一轮日',
-				amount: 12,
-				unit_price: 137,
-				create_dt: 1231232333,
-			}
-			
-		];
-
 		return {
 			groups: [],
 			page: 1,
@@ -28,18 +16,19 @@ module.exports = React.createClass({
 	},
 	listGroups:function(){
 		var data = {
-			page : 1
+			page : 2
 		};
 		this.setState({
             		query_flag:'yes',
             		button_name:'加载中'
             	});
 		$.ajax({
-            type:'get',
+            type:'post',
             url:'/api/v1/groups',
             data:data
         }).done(function (resp) {
-        	if(resp.status == 201){
+        	console.log(resp);
+        	if(resp.status === 200){
         		this.setState({
             		groups:resp.groups,
             		pager_display:true,
@@ -78,9 +67,9 @@ module.exports = React.createClass({
 		};
 		return (
 			<div>
-				<div>{groupComps}</div>
-				<div className='col-md-12'>
-					<div className='col-md-offset-5'><Pager {...pager_props}/></div>
+				<div className="row-fluid hideInIE8 circleStats">{groupComps}</div>
+				<div className='row-fluid'>
+					<div><Pager {...pager_props}/></div>
 				</div>
 			</div>
 
