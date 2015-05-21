be5680a2c2d5de196874ea55cf557f251f877e74
XingKaiXin <xingkaixin@gmail.com>
2015-05-21 16:39:02 +0800

    group item have 3 style by mod 3

--- static/js/GroupsApp/GroupItem.js
+++ static/js/GroupsApp/GroupItem.js
@@ -5,21 +5,36 @@ module.exports = React.createClass({
 	render:function(){
 		var group = this.props.group;
 		var detail_link = '#/group/'+group.id;
+        var mod = group.id%3;
+        var stylecolor = "greenDark";
+        console.log(mod);
+        if(mod == 0){
+            stylecolor = "greenDark";
+        }else if(mod == 1){
+            stylecolor = "green";
+        }else{
+            stylecolor = "greenLight";
+        };
+        stylecolor = "circleStatsItemBox " + stylecolor;
 		return(
-			<div className="col-md-3">
-                <div className="thumbnail">
-                    <img alt="230x200" src="/static/imgs/groups/230x200.gif" />
-                     <div className="caption">
-                        <h3>Product AAAAAAAA</h3>
-                        <p>Cras justo odio, dapibus ac facapibus ac faapibus a
-                        c faapibus ac faapibus ac faapibus ac failisis in, egestas eget quam.
-                         Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultr
-                         Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultr
-                         icies vehicula ut id elit.</p>
-                         <h4>剩余数量：15</h4>
-                         <h4>到手单价：30</h4>
-                        <p><a href={detail_link} className="btn btn-primary">View</a></p>
-                        <p className="momenttime">5天前</p>
+			<div className="span2">
+                <div className={stylecolor}>
+                    <div className="header">{group.title}</div>
+                    <div className="circleStat">
+                        <a href={detail_link}>
+                            <img alt="221x125" src="/static/imgs/groups/221x125.gif" />
+                        </a>
+                    </div>
+                    <div className="footer">
+                        <span className="count">
+                            <span class="unit">到手价：￥</span>
+                            <span className="number">{group.unit_price}</span>
+                        </span>
+                        <span className="sep"> | </span>
+                        <span className="count">
+                            <span class="unit">剩余数量：</span>
+                            <span className="number">{group.total_qty}</span>
+                        </span>
                     </div>
                 </div>
 			</div>
