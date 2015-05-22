75b8b4da8b1dc67018da886eef40fcfdbee87a27
xingkaixin <xingkaixin@gmail.com>
2015-05-23 01:28:46 +0800

    init orderitem

--- static/js/OrderComponents/OrderItem.js
+++ static/js/OrderComponents/OrderItem.js
@@ -4,13 +4,26 @@ var React = require('react');
 
 module.exports = React.createClass({
 	render:function(){
-		var order = this.props.order;
 		return (
 			<div>
-				{order.id}
-				
-			</div>
+				<div className="box-header" data-original-title>
+					<h2>
+						<i className="halflings-icon user"></i>
+						<span className="break"></span>
+						订单: {this.props.order.id}
+						</h2>	
+				</div>
 
+				<div className="box-content">
+					<div className="row-fluid">
+						订单的一些信息啊什么的就放这里
+					</div>
+
+
+
+
+				</div>
+			</div>
 		)
 	}
 })
