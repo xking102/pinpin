02eb1c54265a6df1fb6d51b51e0cc813569d350c
xingkaixin <xingkaixin@gmail.com>
2015-05-23 01:28:32 +0800

    complete MyOrderApp

--- static/js/MyOrdersApp/OrderDetailApp.js
+++ static/js/MyOrdersApp/OrderDetailApp.js
@@ -2,25 +2,47 @@ var React = require('react');
 
 
 module.exports = React.createClass({
+	getInitialState:function(){
+		return {
+			order: []
+		}
+	},
+	listOrders:function(){
+		$.ajax({
+            type:'get',
+            url:'/api/v1/orders/'+this.props.oid,
+            datetype:'json'
+        }).success(function (resp) {
+        	console.log(resp);
+        	this.setState({
+            		order:resp.order
+            	});
+        }.bind(this));
+	},
+	componentDidMount : function(){
+		this.listOrders();
+	},
+	componentWillReceiveProps  : function(){
+		this.listOrders();
+	},
 	render:function(){
 		return (
 			<div className="row-fluid sortable">
 
 				<div className="box span12">
 					<div className="box-header" data-original-title>
-						<h2><i className="halflings-icon user"></i><span className="break"></span>Orders</h2>	
+						<h2>
+							<i className="halflings-icon user"></i>
+							<span className="break"></span>
+							订单编号:{this.props.oid}
+						</h2>	
 					</div>
 
 					<div className="box-content">
+						
 						<div className="row-fluid">
-							<div>id</div>
-							<div>title</div>
-							<div>price</div>
-							<div>Actions</div>
-								
-						</div>
-						<div className="row-fluid">
-								<div>{this.props.oid}</div>
+								<div>￥ {this.state.order.total_price}</div>
+								<div>放些有的没的~~~~</div>
 						</div>
 						
 
--- static/js/MyOrdersApp/OrderItem.js
+++ static/js/MyOrdersApp/OrderItem.js
@@ -2,10 +2,21 @@ var React = require("react");
 
 
 module.exports = React.createClass({
-	handleonClick:function(){
-        console.log(this.props.order.id);
-    },
+    
     render:function(){
+        var status = this.props.order.status;
+        var styleObj={
+            display: 'block',
+         };
+        if(status==25){
+            styleObj={
+                display: 'none',
+            };
+        }else{
+            styleObj={
+                
+            };
+        }
 		return(
 			<div>
                 <div className="row-fluid">
@@ -13,11 +24,11 @@ module.exports = React.createClass({
                     <div className="span3 center">{this.props.order.gid}</div>
                     <div className="span3 center">{this.props.order.unit_price}</div>
                     <div className="span3 center">
-                        <a className="btn btn-success" href={'#order/'+this.props.order.id}>
+                        <a className="btn btn-info" href={"order#/"+this.props.order.id}>
                             <i className="halflings-icon white zoom-in"></i>
                         </a>
-                        <a className="btn btn-danger" onClick={this.handleonClick} href="#">
-                            <i className="halflings-icon white trash"></i>
+                        <a className="btn btn-danger" style={styleObj} href={"order#/"+this.props.order.id+"/pay"}>
+                            <i className="halflings-icon white ok"></i>
                         </a>
                     </div>
                 </div>
