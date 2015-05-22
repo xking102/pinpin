46642af7fac6e27f87f62b4206eecb7ca09ebaa6
XingKaiXin <xingkaixin@gmail.com>
2015-05-22 14:09:49 +0800

    add pay route

--- static/js/my_order_list.js
+++ static/js/my_order_list.js
@@ -3,17 +3,21 @@ var Router = require('react-router');
 var { Route, DefaultRoute, RouteHandler, Link } = Router;
 var MyOrdersApp = require('./MyOrdersApp/OrdersApp');
 var OrderDetailApp = require('./MyOrdersApp/OrderDetailApp');
+var PayApp = require('./PayApp/PayApp');
+
 
 var App = React.createClass({
 
   contextTypes: {
     router: React.PropTypes.func.isRequired
   },
-
   render: function () {
+    var params = this.context.router.getCurrentParams();
+    console.log(this.context.router.getCurrentRoutes()[1].name);
+    var oid = params.oid;
     return (
       <div>
-          <RouteHandler/>
+          <RouteHandler oid={oid}/>
       </div>
     );
   }
@@ -26,6 +30,7 @@ var routes = (
   <Route handler={App}>
     <DefaultRoute handler={MyOrdersApp}/>
     <Route name="order" path="order/:oid" handler={OrderDetailApp} />
+    <Route name="pay" path="pay/:oid" handler={PayApp} />
   </Route>
 );
 
