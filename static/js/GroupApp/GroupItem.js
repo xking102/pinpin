8fceb5973a8b963e85a2f01b4460a942b819da09
xingkaixin <xingkaixin@gmail.com>
2015-05-23 22:27:42 +0800

    add timelime~

--- static/js/GroupApp/GroupItem.js
+++ static/js/GroupApp/GroupItem.js
@@ -55,75 +55,222 @@ module.exports = React.createClass({
           }.bind(this));
           }  
       },
+
   render:function(){
     var group = this.props.group;
-        var styleObj={
+    var styleObj={
             width: '45px',
-        }
+        };
     return(
-      <div>
+    <div>
   
-              <div className="box span6">
-                <div className="box-header" data-original-title>
-                  <h2>
-                  <i className="halflings-icon picture"></i>
-                  <span className="break"></span>
-                  {group.title}</h2>
-                </div>
-            
-                <div className="box-content">
-                  <img className="grayscale" src="/static/imgs/groups/2.png" alt="Sample Image 1"/>
-                </div>
-            
+  <div className="row-fluid">
+    
+    <div className="span7 noMarginRight">
+      
+      <div className="row-fluid">
+        <div className="box span7">
+          <div className="box-header" data-original-title>
+            <h2>
+            <i className="halflings-icon picture"></i>
+            <span className="break"></span>
+            {group.title}</h2>
+          </div>
+          
+          <div className="box-content">
+            <img className="grayscale" src="/static/imgs/groups/2.png" alt="Sample Image 1"/>
+          </div>
+          
+        </div>
+        <div className="box span5 noMarginRight">
+          
+          <div className="box-header" data-original-title>
+            <h2>
+            <i className="halflings-icon list-alt"></i>
+            <span className="break"></span>
+            购买信息</h2>
+          </div>
+          
+          <div className="box-content">
+            <h3>到手价：{group.unit_price}</h3>
+            <h3>原价：{group.list_price}</h3>
+            <h3>剩余数：{group.total_qty}</h3>
+            <div className="row-fluid">
+              
+              <div className="span2">
+                <input  style={styleObj} type="number" value={this.state.reqnum} onChange={this.handlerReqnum} />
               </div>
-            
-            
-              <div className="box span4">
-                
-                <div className="box-header" data-original-title>
-                  <h2>
-                  <i className="halflings-icon list-alt"></i>
-                  <span className="break"></span>
-                  购买信息</h2>
-                </div>
-            
-                <div className="box-content">
-                  <h3>到手价：{group.unit_price}</h3>
-                  <h3>原价：{group.list_price}</h3>
-                  <h3>剩余数：{group.total_qty}</h3>
-                  <div className="row-fluid">
-            
-                    <div className="span2">
-                      <input  style={styleObj} type="number" value={this.state.reqnum} onChange={this.handlerReqnum} />
-                    </div>
-            
-                    <div className="span2">
-                      <a href="#" onClick={this.handlerOrder} className="btn btn-danger">{this.state.btn_buy_name}</a>
-                    </div>
-                    
-                  </div>
-            
-                </div>
-                
+              
+              <div className="span2">
+                <a href="#" onClick={this.handlerOrder} className="btn btn-danger">{this.state.btn_buy_name}</a>
               </div>
-
-
-              <div className="box span10">
-                <div className="box-header" data-original-title>
-                  <h2>
-                  <i className="halflings-icon book"></i>
-                  <span className="break"></span>
-                  详细描述</h2>
-                </div>
-            
-                <div className="box-content">
-                 {group.desc}
-                </div>
-            
-              </div>
-
               
             </div>
+            
+          </div>
+          
+        </div>
+      </div>
+      <div className="row-fluid">
+        <div className="box span12">
+          <div className="box-header" data-original-title>
+            <h2>
+            <i className="halflings-icon book"></i>
+            <span className="break"></span>
+            详细描述</h2>
+          </div>
+          
+          <div className="box-content">
+            {group.desc}
+          </div>
+          
+        </div>
+      </div>
+    </div>
+    <div className="span5 noMarinLeft">
+      <div className="dark">
+        
+        <h1>Timeline</h1>
+        
+        <div className="timeline">
+          <div className="timeslot">
+            
+            <div className="task">
+              <span>
+                <span className="type">appointment</span>
+                <span className="details">
+                  Dennis Ji at Bootstrap Metro Dashboard HQ
+                </span>
+                <span>
+                  remaining time
+                  <span className="remaining">
+                    3h 38m 15s
+                  </span>
+                </span>
+              </span>
+              <div className="arrow"></div>
+            </div>
+            <div className="icon">
+              <i className="icon-map-marker"></i>
+            </div>
+            <div className="time">
+              3:43 PM
+            </div>
+            
+          </div>
+          
+          <div className="clearfix"></div>
+          <div className="timeslot alt">
+            
+            <div className="task">
+              <span>
+                <span className="type">phone call</span>
+                <span className="details">
+                  Dennis Ji
+                </span>
+                <span>
+                  remaining time
+                  <span className="remaining">
+                    3h 38m 15s
+                  </span>
+                </span>
+              </span>
+              <div className="arrow"></div>
+            </div>
+            <div className="icon">
+              <i className="icon-phone"></i>
+            </div>
+            <div className="time">
+              3:43 PM
+            </div>
+            
+          </div>
+          <div className="timeslot">
+            
+            <div className="task">
+              <span>
+                <span className="type">mail</span>
+                <span className="details">
+                  Dennis Ji
+                </span>
+                <span>
+                  remaining time
+                  <span className="remaining">
+                    3h 38m 15s
+                  </span>
+                </span>
+              </span>
+              <div className="arrow"></div>
+            </div>
+            <div className="icon">
+              <i className="icon-envelope"></i>
+            </div>
+            <div className="time">
+              3:43 PM
+            </div>
+            
+          </div>
+          
+          <div className="timeslot alt">
+            
+            <div className="task">
+              <span>
+                <span className="type">deadline</span>
+                <span className="details">
+                  Fixed bugs
+                </span>
+                <span>
+                  remaining time
+                  <span className="remaining">
+                    3h 38m 15s
+                  </span>
+                </span>
+              </span>
+              <div className="arrow"></div>
+            </div>
+            <div className="icon">
+              <i className="icon-calendar"></i>
+            </div>
+            <div className="time">
+              3:43 PM
+            </div>
+            
+          </div>
+          
+          <div className="timeslot">
+            
+            <div className="task">
+              <span>
+                <span className="type">appointment</span>
+                <span className="details">
+                  Dennis Ji at Bootstrap Metro Dashboard HQ
+                </span>
+                <span>
+                  remaining time
+                  <span className="remaining">
+                    3h 38m 15s
+                  </span>
+                </span>
+              </span>
+              <div className="arrow"></div>
+            </div>
+            <div className="icon">
+              <i className="icon-map-marker"></i>
+            </div>
+            <div className="time">
+              3:43 PM
+            </div>
+            
+          </div>
+          
+          <div className="clearfix"></div>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+  
+</div>
     )   
   }
 })
