ea259f836512ed551be0680b5ce6703a51444fb1
Kevin <xingkaixin@gmail.com>
2015-06-03 00:28:33 +0800

    add new method

--- view/group/group.py
+++ view/group/group.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from flask import Blueprint, request, session, redirect, url_for, \
-    abort, render_template, flash, current_app
+    abort, render_template, flash, current_app, make_response, jsonify
 from sqlalchemy import or_
 from control import pinpin
 from control.pinpin import statusRef
 from module.group.group import Group
+from module.order.order import Order
 from form.group.group import newGroupForm
 from app import db
 from view.workflow.workflow import Push_Steps
@@ -43,3 +44,22 @@ def group_processing(gid):
 @group.route('/u/group')
 def list_u_groups():
     return render_template("./group/mygroups.html")
+
+
+# list a group confirm orders
+@group.route('/u/group/<int:gid>')
+def list_u_groupsOrder(gid):
+    if session.get('logged_in'):
+        g = Group.query.get(gid)
+        if g and g.create_userid == session.get('logged_id'):
+            orders = Order.query.filter_by(status=20,gid=gid).all()
+            return make_response(jsonify({"orders": [order.to_json for order in orders]}), 200)
+        return make_response('not exist', 404)
+    return make_response('need login', 401)
+
+
+
+
+
+
+
