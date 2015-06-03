bb1087cebbe472dcbc6a8540b82bbdca4325f876
XingKaiXin <xingkaixin@gmail.com>
2015-06-03 19:55:15 +0800

    delivery method

--- view/group/group.py
+++ view/group/group.py
@@ -31,14 +31,13 @@ def add_group():
     return render_template('./group/add.html', error=error, form=form)
 
 
-
 def group_processing(gid):
-	g = Group.query.get(gid)
-	if g:
-		if g.confirm_qty==g.total_qty and g.status==statusRef.GROUP_PUBLISH:
-			g.status=statusRef.GROUP_PROCESSING
-			g.save
-			Push_Steps(1,gid)
+    g = Group.query.get(gid)
+    if g:
+        if g.confirm_qty == g.total_qty and g.status == statusRef.GROUP_PUBLISH:
+            g.status = statusRef.GROUP_PROCESSING
+            g.save
+            Push_Steps(1, gid)
 
 
 # list user orders
@@ -53,11 +52,10 @@ def list_u_groupsOrder(gid):
     if session.get('logged_in'):
         g = Group.query.get(gid)
         if g and g.create_userid == session.get('logged_id'):
-            orders = Order.query.filter_by(status=20,gid=gid).all()
+            orders = Order.query.filter_by(status=20, gid=gid).all()
             return make_response(jsonify({"orders": [order.to_json for order in orders]}), 200)
         return make_response('not exist', 404)
     return make_response('need login', 401)
-
 
 
 # push a group status from PROCESSING(15) to CONFRIM(20)
@@ -65,15 +63,24 @@ def list_u_groupsOrder(gid):
 def deliver_u_group(gid):
     if session.get('logged_in'):
         uid = session.get('logged_id')
-        g = Group.query.filter_by(status=statusRef.GROUP_PROCESSING,id=gid,create_userid=uid).first()
+        g = Group.query.filter_by(
+            status=statusRef.GROUP_PROCESSING, id=gid, create_userid=uid).first()
         if g:
-            orders = Order.query.filter_by(gid=gid,status=statusRef.GROUP).all()
+            orders = Order.query.filter_by(
+                gid=gid, status=statusRef.GROUP).all()
             if orders:
+                pending = []
                 for o in orders:
-                    trans = Transport.query.filter_by().all()
-            return make_response('not exist', 404)    
+                    trans = Transport.query.filter_by(oid=o.id).first()
+                    if len(trans.transcode) == 0 or len(trans.transorg) == 0:
+                        pending.append(trans.id)
+                if len(pending)==0:
+                    g.status = statusRef.GROUP_CONFIRM
+                    for o in orders:
+                        o.status = statusRef.ORDER_PENDING
+                    db.session.commit()
+                    return make_response(jsonify({'messages':'ok','status':'succ'}), 200) 
+                return make_response(jsonify({'messages':'todo','status':'fail'}), 200) 
+            return make_response('not exist', 404)
         return make_response('not exist', 404)
     return make_response('need login', 401)
-
-
-
