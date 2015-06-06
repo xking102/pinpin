adecafc16bf84fabe1e6429daafe22ab9c7017b6
Kevin <xingkaixin@gmail.com>
2015-06-06 17:20:42 +0800

    add a group and upload a image

--- form/group/group.py
+++ form/group/group.py
@@ -5,8 +5,9 @@ from flask import session
 from flask_wtf import Form
 from flask_wtf.html5 import EmailField
 from wtforms import StringField, PasswordField, SubmitField, validators, \
-    TextAreaField, DecimalField, IntegerField
+    TextAreaField, DecimalField, IntegerField, FileField
 from wtforms.validators import DataRequired, Email, InputRequired, NumberRange
+from werkzeug import secure_filename
 from module.group.group import Group
 from view.workflow.workflow import init_group_wf
 from control import pinpin
@@ -15,6 +16,7 @@ from app import db
 
 
 class newGroupForm(Form):
+    image = FileField(u'商品照片')
     title = StringField(u'标题', [InputRequired()])
     desc = TextAreaField(u'描述', [InputRequired()])
     unit_price = DecimalField(
@@ -31,6 +33,12 @@ class newGroupForm(Form):
         unit_price = self.unit_price.data
         list_price = self.list_price.data
         total_qty = self.total_qty.data
+        image = self.image.data
+        filename = secure_filename(image.filename)
+        pre = 'static/imgs/groups/group-img-' + \
+            str(pinpin.getCurTimestamp()) + \
+            '-'
+        image.save(pre + filename)
         group = Group()
         group.title = title
         group.desc = desc
