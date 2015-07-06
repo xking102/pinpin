b01fae41c81df16e8b59a3d1bf7fb31272e488f7
XingKaiXin <xingkaixin@gmail.com>
2015-07-06 11:11:51 +0800

    new UploadCheckFile

---
+++ static/js/MyGroup/UploadCheckFiles.js
@@ -0,0 +1,193 @@
+var React = require("react");
+
+var mui = require('material-ui');
+var {TextField, Paper, RaisedButton, Snackbar} = mui;
+var Dropzone = require('react-dropzone');
+var LoadingMask = require("../Components/LoadingMask");
+
+
+module.exports = React.createClass({
+    contextTypes: {
+        router: React.PropTypes.func.isRequired
+    },
+    _checkField:function(){
+
+        var errorImages = this.state.errorImages;
+    
+        if(errorImages.length>0){
+          this.setState({
+            errorSubmit:'请填写完整'
+          });
+          return true;
+        }else{
+          this.setState({
+            errorSubmit:''
+          });
+          return false;
+        }
+    },
+    PostFiles:function(num){
+        var params = this.context.router.getCurrentParams();
+        var gid = params.gid;
+        if(num){
+          return '';
+        }
+        this.setState({
+          submitflag:true
+        });
+        this.refs.loading.show();
+        var formData = new FormData();
+        for (var i=0;i<this.state.images.length;i++){
+          var file = this.state.images[i];
+          formData.append('photos',file);
+        }
+        formData.append('images',this.state.images);
+    
+        $.ajax({
+          url      : '/groups/check/' + gid,
+          type     : 'POST',
+          dataType: 'json',
+          cache: false,
+          contentType: false,
+          processData: false,
+          data:formData,
+          success: function(resp) {
+            console.log('succ');
+            this._onHandleCancel();
+            this.refs.loading.dismiss();
+            this.setState({
+              submitflag:false
+            });
+            this.refs.snackbar_succ.show();
+            this.context.router.goBack();
+          }.bind(this),
+    
+          error: function(xhr, status, err) {
+            console.error(status, err.toString);
+            this.setState({
+              submitflag:false
+            });
+            this.refs.loading.dismiss();
+          }.bind(this)
+        });
+    },
+    getInitialState:function(){
+      return {
+        images:[],
+        flag:true,
+        errorImages:'别忘了上传图片',
+        errorSubmit:'',
+        submitflag:false,
+      }
+    },
+    _handleForm:function(e){
+      e.preventDefault();
+      var num = this._checkField();
+      this.PostGroup(num);
+    },
+
+  
+
+    render: function () {
+      var params = this.context.router.getCurrentParams();
+      var gid = params.gid;
+      var showSubmitError = this.state.errorSubmit.length==0?'':
+      <div style={{marginTop:'50px'}} className="alert alert-error">{this.state.errorSubmit}</div>;
+      return (
+        <div style={{marginLeft:'20%',marginRight:'20%'}}  >
+            <Paper zDepth={3}>
+            <div style={{marginLeft:'10px',marginRight:'10px',paddingBottom:'20px'}}>
+            {gid}
+    
+    
+            <form  onSubmit={this._handleForm} ref="addGroupForm" name="addGroup" encType="multipart/form-data" method="POST">
+              
+    
+              <br/>
+    
+              <Dropzone ref="dropzone" onDrop={this._onDrop} size={150} >
+                <div>你可以拖拽多张图片到此处或者双击选择多张图片</div>
+              </Dropzone>
+              {this.showFiles()}
+    
+              <br/>
+    
+    
+              <div className="pull-right">
+              <a className="btn btn-link" onClick={this._onHandleCancel}>重新填写</a>
+              &nbsp;
+              <RaisedButton
+              label="添加"
+              secondary={true}
+              disabled={this.state.submitflag}
+              />
+    
+             
+    
+               <Snackbar
+                  ref="snackbar_succ"
+                  message={'发布成功'}
+              />
+            
+              </div>
+    
+              {showSubmitError}
+    
+    
+            </form>
+    
+    
+            </div>
+            </Paper>
+            <LoadingMask ref="loading"/>
+        </div>
+      );
+    },
+
+
+    _onDrop:function(imgs){
+      if(imgs.length<6){
+        this.setState({
+          images:imgs,
+          errorImages:''
+        });
+      }else{
+        this.setState({
+          images:'',
+          errorImages:'至多可以上传5张图片'
+        });
+      }
+    },
+    
+    showFiles: function () {
+      if (this.state.images.length <= 0) {
+        return <div style={{marginTop:'10px'}} className="alert alert-error">{this.state.errorImages}</div>;
+      }
+    
+      var images = this.state.images;
+    
+      return (
+        <div>
+          <h3>预览</h3>
+            {[].map.call(images, function (f, i) {
+              return <img style={{marginRight:'3px',marginBottom:'3px'}}width={100} height={100} key={i} src={f.preview} />;
+            })}
+        </div>
+      );
+    },
+
+
+
+  _onHandleCancel:function(){
+    this.refs.addGroupForm.getDOMNode().reset();
+    this.setState({
+      images:[],
+      errorImages:'别忘了上传图片',
+      errorSubmit:''
+    })
+  },
+
+ 
+
+
+})
