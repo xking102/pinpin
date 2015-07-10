90317b3d36f0961fc6d3a2182bf3ad2749c63ec4
XingKaiXin <xingkaixin@gmail.com>
2015-07-10 18:03:09 +0800

    admin console is admin user can route

--- admin/MyModelView.py
+++ admin/MyModelView.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from flask_admin.contrib.sqla import ModelView
-from flask import session, abort
+from flask.ext.admin import AdminIndexView, expose
+from flask import session, abort, redirect
+
 
 class MyModelView(ModelView):
 
     def is_accessible(self):
-        if session.get('logged_in'):
+        if session.get('isAdmin') or session.get('logged_id') == 1:
             return True
         return False
 
@@ -18,3 +20,10 @@ class MyModelView(ModelView):
             abort(404)
 
 
+class MyAdminIndexView(AdminIndexView):
+
+    @expose('/')
+    def index(self):
+        if session.get('isAdmin') or session.get('logged_id') == 1:
+            return super(MyAdminIndexView, self).index()
+        return redirect('/')
