06ef87107a7356c55fa5f2e9a096ec40af9b1788
XingKaiXin <xingkaixin@gmail.com>
2015-07-10 16:26:00 +0800

    add Flask-Admin

--- myapp.py
+++ myapp.py
@@ -2,12 +2,16 @@
 # -*- coding: utf-8 -*-
 
 
-from flask import Flask, render_template, Blueprint
+from flask import Flask, render_template, Blueprint, session
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.admin import Admin, BaseView, expose
+from flask_admin.contrib.sqla import ModelView
+from flask_admin.contrib.fileadmin import FileAdmin
 from flask_bootstrap import Bootstrap
 from flask.ext.restful import Api, Resource
 from werkzeug.contrib.fixers import ProxyFix
 import myapp
+import os.path as op
 
 
 DEBUG = True
@@ -15,25 +19,56 @@ DEBUG = True
 SECRET_KEY = 'development key'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///server.db'
 
+
+# class MyView(BaseView):
+
+#     @expose('/')
+#     def index(self):
+#         print 'ssss', session.get('logged_in')
+#         if session.get('logged_in'):
+#             return self.render('./admin/index1.html')
+#         print 'dddd', session.get('logged_in')
+#         return self.render("./group/index.html")
+
+
 app = Flask(__name__)
 app.config.from_object(__name__)
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
 db = SQLAlchemy(app)
+admin = Admin(app, name='PinPin Admin', template_mode='bootstrap3')
 app.wsgi_app = ProxyFix(app.wsgi_app)
 Bootstrap(app)
 
 
-# from pinpin.user.view import user
-# from pinpin.order.view import order
-# from pinpin.shopcart.view import shopcart
-# from pinpin.admin.view import admin
-# from pinpin.search.view import search
+from admin.MyModelView import MyModelView
+from module.user.user import User as UserModule
+from module.user.useraddress import UserAddress as UserAddressModule
+from module.user.InviteCode import InviteCode as InviteCodeModule
+from module.group.group import Group as GroupModule
+from module.image.image import Image as ImageModule
+from module.order.order import Order as OrderModule
+from module.transport.transport import Transport as TransportModule
+admin.add_view(
+    MyModelView(UserModule, db.session, endpoint='info', category='User'))
+admin.add_view(
+    MyModelView(UserAddressModule, db.session, endpoint='address', category='User'))
+admin.add_view(
+    MyModelView(InviteCodeModule, db.session, endpoint='invitecode', category='User'))
+admin.add_view(
+    MyModelView(GroupModule, db.session, endpoint='group', category='Group'))
+admin.add_view(
+    MyModelView(ImageModule, db.session, endpoint='image', category='Group'))
+admin.add_view(
+    MyModelView(OrderModule, db.session, endpoint='order', category='Order'))
+admin.add_view(
+    MyModelView(TransportModule, db.session, endpoint='transport', category='Order'))
 
-from view.group.group import group
-from view.user.user import user
-from view.order.order import order
-from view.other.other import other
+
+from view.group.group import groupview
+from view.user.user import userview
+from view.order.order import orderview
+from view.other.other import otherview
 
 from api.group.group import Groups, Group, MyGroups, MyGroup
 from api.order.order import Orders, Order, MyOrders
@@ -42,13 +77,10 @@ from api.user.useraddress import MyAddresses, MyAddress
 from api.transport.transport import MyTransport, MyTransports
 
 
-app.register_blueprint(user)
-app.register_blueprint(group)
-app.register_blueprint(order)
-app.register_blueprint(other)
-# app.register_blueprint(shopcart, url_prefix='/shopcart')
-# app.register_blueprint(admin, url_prefix='/admin')
-# app.register_blueprint(search, url_prefix='/search')
+app.register_blueprint(userview)
+app.register_blueprint(groupview)
+app.register_blueprint(orderview)
+app.register_blueprint(otherview)
 
 
 """
