3cdaf203a2d67c5f569797227e38e01f00af9ed8
XingKaiXin <xingkaixin@gmail.com>
2015-07-14 17:27:43 +0800

    modify

--- utils/UnicodeTranslate.py
+++ utils/UnicodeTranslate.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
 
 
-def trancChar(v_str,v_len):
+def trancChar(v_str,v_len,v_full=True):
+	'''
+	v_str translate string
+	v_len substring length
+	v_full return full translated string or "..." 
+	'''
 	try:
 		decode_str = v_str.decode('utf-8')
+		print len(decode_str) 
 		if len(decode_str) > v_len:
-			return decode_str[0:v_len].encode('utf-8')+'...'
+			if v_full:
+				return decode_str[0:v_len].encode('utf-8').decode('utf-8')
+			return decode_str[0:v_len].encode('utf-8')+'...'.decode('utf-8')
 		else:
 			return v_str
 	except Exception as e:
