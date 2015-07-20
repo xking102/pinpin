37bb02ac7b1dccf180099d9b8a052b8064ab68ce
XingKaiXin <xingkaixin@gmail.com>
2015-07-21 01:31:54 +0800

    add callback url

--- view/order/order.py
+++ view/order/order.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from flask import Blueprint, request, redirect, url_for, \
-    abort, render_template, flash, current_app, make_response
+    abort, render_template, flash, current_app, make_response, jsonify
 from sqlalchemy import or_
 from control import pinpin
 from control.pinpin import statusRef
 from module.order.order import Order
 from module.group.group import Group
+from module.transport.transport import Transport
 from myapp import db
 from view.workflow.workflow import Push_Steps
 from view.group.group import group_processing, tellGroupThatOrderisConfirmed
 from flask.ext.login import current_user
+from view.alipayapp.alipayapp import alipay_pay_on_web
 
 orderview = Blueprint('orderview', __name__)
 
@@ -29,18 +31,31 @@ def order_pay(oid):
     if current_user.is_authenticated():
         uid = current_user.id
         order = Order.query.get(oid)
-        if order and order.create_userid == uid and is_pay(oid):
+        if order and order.create_userid == uid and order.status == statusRef.ORDER_APPORVED:
+            # g = Group.query.get(order.gid)
+            # g.req_qty -= order.req_qty
+            # g.confirm_qty += order.req_qty
+            # g.save
+            # group_processing(g.id)
+            # order.status = statusRef.ORDER_PAIED
+            # order.save
+            # Push_Steps(2, oid)
+            # return make_response('payment succ', 201)
             g = Group.query.get(order.gid)
-            g.req_qty -= order.req_qty
-            g.confirm_qty += order.req_qty
-            g.save
-            group_processing(g.id)
-            order.status = statusRef.ORDER_PAIED
-            order.save
-            Push_Steps(2, oid)
-            return make_response('payment succ', 201)
-        return make_response('no permission', 404)
-    return make_response('need login', 401)
+            t = Transport.query.filter_by(oid=order.id).first()
+            param ={
+                'out_trade_no':order.trade_no,
+                'subject':g.title,
+                'price':order.unit_price,
+                'quantity':order.req_qty,
+                'receive_name':t.reciver,
+                'receive_address':t.address_line1,
+                'receive_mobile':t.tel
+            }
+            alipayurl = alipay_pay_on_web(**param)
+            return make_response(jsonify({'url': alipayurl}), 200)
+        return make_response(jsonify({'messages': 'permission'}), 404)
+    return make_response(jsonify({'messages': 'need login'}), 401)
 
 # user cancel the order
 
